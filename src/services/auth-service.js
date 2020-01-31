e3952e564dc6cf9ea175177a3f5813fc7d1b10f6
David Mitjana Castro <d.mitjana@gmail.com>
2020-01-31 20:00:17 +0100

    add auth service

---
+++ src/services/auth-service.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+class Auth {
+  constructor() {
+    this.auth = axios.create( {
+      baseURL: 'http://localhost:5000/api/auth',
+      withCredentials: true,
+    });
+  }
+
+  signup(user) {
+    const { email, password } = user;
+    return this.auth.post('/signup', { email, password })
+            .then(({ data }) => data);
+  }
+
+  login(user) {
+    const { email, password } = user;
+    return this.auth.post('/login', { email, password })
+            .then(({ data }) => data);
+  }
+
+  logout() {
+    return this.auth.post('/logout')
+            .then(({ response }) => response.data);
+  }
+}
+
+const authService = new Auth();
+
+export default authService;
