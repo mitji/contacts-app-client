99ac0b432b0f940914670242cbbc6fe6bae6e270
David Mitjana Castro <d.mitjana@gmail.com>
2020-02-03 22:51:26 +0100

    add logout button

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -3,8 +3,12 @@ import { withAuth } from './../services/AuthProvider';
 
 class Contacts extends Component {
   render() {
+    console.log(this.props)
     return (
-      <h1>Contacts page</h1>
+      <div>
+        <h1>Contacts page</h1>
+        <button onClick={this.props.logout}>Logout</button>
+      </div>
     )
   }
 }
