c5ed1a5ea29cd8f52965ed46ae61e3141cbadcd0
David Mitjana Castro <d.mitjana@gmail.com>
2020-02-04 23:52:53 +0100

    add action for letter filter

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -2,3 +2,13 @@ export const addAllContacts = ( allContacts ) => ({
   type: 'ADD_ALL_CONTACTS',
   payload: allContacts
 })
+
+export const addContactDetails = ( contact ) => ({
+  type: 'ADD_CONTACT_DETAILS',
+  payload: contact
+})
+
+export const filterByLetter = ( letter ) => ({
+  type: 'FILTER_BY_LETTER',
+  payload: letter
+})
