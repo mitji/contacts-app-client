2c824fa09a70455d427b3a43e16799c59998b1bc
David Mitjana Castro <d.mitjana@gmail.com>
2020-02-05 18:53:04 +0100

    fix current page numbe

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './../styles/pagination.scss';
 
 const Pagination = (props) => {
-
+  let currentPage = props.currentPage;
+  if(props.numPages === 0) currentPage = 0;
+  if(currentPage > props.numPages) currentPage = 1;
   return (
     <ul className="pagination">
-      { props.currentPage > 1 
-        ? <li><button onClick={() => props.nextPage(props.currentPage-1)}>Previous</button></li>
+      { currentPage > 1 
+        ? <li><button onClick={() => props.nextPage(currentPage-1)}>Previous</button></li>
         : <li><button>Previous</button></li>
       }
-      <li>{props.currentPage} / {props.numPages}</li>
-      { props.currentPage < props.numPages
-        ? <li><button onClick={() => props.nextPage(props.currentPage+1)}>Next</button></li>
+      <li>{currentPage} / {props.numPages}</li>
+      { currentPage < props.numPages
+        ? <li><button onClick={() => props.nextPage(currentPage+1)}>Next</button></li>
         : <li><button>Next</button></li>
       }
       
